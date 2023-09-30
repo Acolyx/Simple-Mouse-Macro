67f534c23e6c7f30236fe909203f44afd7a0b8e3
Acolyx <a.ayd_@outlook.com>
2023-09-30 11:30:31 +0300

    Initial commit

---
+++ Spraysoft/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace Spraysoft
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Cascadia Code", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(230, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(98, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Cascadia Code", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(230, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(98, 38);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(84, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Delay :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(4, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Multiplier :";
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 35);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "mPos Rate :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Cascadia Code", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(230, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(98, 38);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 185);
+            this.ControlBox = false;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AutoClick";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+
+    }
+}
+
---
+++ Spraysoft/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Spraysoft
+{
+  
+    public partial class Form1 : Form
+    {
+        int delay = Properties.Settings.Default.delay;
+        int mpospx = 0;
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+        int multiplier = Properties.Settings.Default.mutliplier;
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData,
+   int dwExtraInfo);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void lclick(int multiplier,int delay)
+        {
+            for (int i = 0; i < multiplier; i++)
+            {
+                mouse_event(0x02, 0, 0, 0, 0);
+                mouse_event(0x04, 0, 0, 0, 0);
+            }
+           
+
+
+        }
+        void rclick(int multiplier, int delay)
+        {
+            for (int i = 0; i < multiplier; i++)
+            {
+                mouse_event(0x08, 0, 0, 0, 0);
+                mouse_event(0x10, 0, 0, 0, 0);
+            }
+           
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = delay.ToString();
+            textBox2.Text = multiplier.ToString();
+        }
+        void cursorp(int mpospx)
+        {
+
+            SetCursorPos(Cursor.Position.X, Cursor.Position.Y + mpospx);
+
+
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (GetAsyncKeyState(Keys.XButton2) == -32768) 
+            {
+                lclick(multiplier, 1);
+                Thread.Sleep(delay);
+                cursorp(mpospx);
+                
+            }
+            if (GetAsyncKeyState(Keys.XButton1) == -32768)
+            {
+                rclick(multiplier, 1);
+                Thread.Sleep(delay);
+                cursorp(mpospx);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                delay = int.Parse(textBox1.Text);
+                Properties.Settings.Default.delay = delay;
+                Properties.Settings.Default.Save();
+            }
+            catch (Exception)
+            {
+
+             
+            }
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                multiplier = int.Parse(textBox2.Text);
+                Properties.Settings.Default.mutliplier = multiplier;
+                Properties.Settings.Default.Save();
+
+            }
+            catch (Exception)
+            {
+
+              
+            }
+
+        }
+       
+        private void textBox3_TextChanged(object sender , EventArgs e)
+        {
+            try
+            {
+                mpospx = Convert.ToInt32(textBox3.Text);
+            }
+            catch (Exception)
+            {
+
+                
+            }
+
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (GetAsyncKeyState(Keys.F7) == -32768)
+            {
+
+                timer1.Enabled = false;
+                Thread.Sleep(3000);
+
+            }
+            if (GetAsyncKeyState(Keys.F8) == -32768)
+            {
+
+
+                timer1.Enabled = true;
+                Thread.Sleep(3000);
+
+
+            }
+        }
+    }
+}
